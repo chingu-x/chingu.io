16976762e242c1f35318e38e865706fbd1e1ce9f
Cheryl Murphy <6191116+cherylli@users.noreply.github.com>
2026-07-14 00:57:39 +1000

    [WIP] index stats section

--- src/components/shared/styles/home/typography.ts
+++ src/components/shared/styles/home/typography.ts
@@ -1,5 +1,6 @@
 export const homeTypography = {
     h1: `text-4xl font-bold`,
+    p: `text-neutral-focus`,
     gradient: `
         bg-[linear-gradient(135deg,var(--primary)_0%,var(--primary-focus)_55%,var(--accent-focus)_100%)]
         bg-clip-text
