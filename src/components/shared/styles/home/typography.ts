e3cf5f611873092805b2cbda5dc1be3ea8a25f83
Cheryl Murphy <6191116+cherylli@users.noreply.github.com>
2026-07-14 19:36:28 +1000

    add heading badge component

--- src/components/shared/styles/home/typography.ts
+++ src/components/shared/styles/home/typography.ts
@@ -1,6 +1,10 @@
+// TODO: maybe move it to index.tsx, or a separate file with all the home page styles
+
 export const homeTypography = {
     h1: `text-4xl font-bold`,
+    h2: `text-2xl font-bold`,
     p: `text-neutral-focus`,
+    pIndented: `text-neutral-focus pl-8`,
     gradient: `
         bg-[linear-gradient(135deg,var(--primary)_0%,var(--primary-focus)_55%,var(--accent-focus)_100%)]
         bg-clip-text
