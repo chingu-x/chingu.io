b02eecfb841da68cb6c9a0a5b3be1b92ab5de056
Cheryl Murphy <6191116+cherylli@users.noreply.github.com>
2026-07-15 20:31:36 +1000

    add role cards

--- src/components/home/heading-badge.tsx
+++ src/components/home/heading-badge.tsx
@@ -1,32 +1,33 @@
+import { cva } from "class-variance-authority";
+import { cn } from "#/lib/utils.ts";
 import { Badge } from "@/components/ui/badge";
-import {cva} from "class-variance-authority";
-import {cn} from "#/lib/utils.ts";
 
-const badgeStyles = cva(`
+const badgeStyles = cva(
+	`
     uppercase
     font-bold
     p-3
-`, {
-    variants: {
-        variant: {
-            default: "text-primary bg-primary/10 ",
-            dark:"",
-        }
-    }
-})
+`,
+	{
+		variants: {
+			variant: {
+				default: "text-primary bg-primary/10 ",
+				dark: "text-primary bg-primary-content/10",
+			},
+		},
+	},
+);
 
 export function HeadingBadge({
-    text,
-    variant = "default",
-    className
-}:{
-    text: string,
-    variant?: "default" | "dark"
-    className?: string
-}){
-    return (
-        <Badge className={cn(badgeStyles({variant}), className)}>
-            {text}
-        </Badge>
-    )
+	text,
+	variant = "default",
+	className,
+}: {
+	text: string;
+	variant?: "default" | "dark";
+	className?: string;
+}) {
+	return (
+		<Badge className={cn(badgeStyles({ variant }), className)}>{text}</Badge>
+	);
 }
