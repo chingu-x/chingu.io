3bb2856d2340c3cf3333381bf1b587b9d5c2a3c2
Cheryl Murphy <6191116+cherylli@users.noreply.github.com>
2026-07-16 11:48:30 +1000

    move padding to section level instead of page level to handle full width section backgrounds

--- src/components/home/content-section.tsx
+++ src/components/home/content-section.tsx
@@ -4,17 +4,20 @@ import type { ReactNode } from "react";
 import { HeadingBadge } from "#/components/home/heading-badge.tsx";
 import { homeTypography as t } from "#/components/shared/styles/home";
 
+const baseSectionStyle = `
+	flex flex-col gap-4 
+	px-content-margin
+	py-10
+`
+
 const defaultSectionStyle = `
-    w-full flex flex-col gap-4 pt-8 pb-4
+	${baseSectionStyle}
+    w-full 
 `;
 export const darkSectionStyles = `
-    flex flex-col 
-    gap-4 
-    mt-8 mb-4 
-    pt-8
-    w-screen ml-[calc(50%-50vw)] mr-[calc(50%-50vw)]
-    pl-content-margin
-    bg-background-100     
+    ${baseSectionStyle}
+    px-content-margin
+    bg-background-100
 `;
 
 export const ContentSection = ({
