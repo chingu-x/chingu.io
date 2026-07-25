c515755a0235bb25f6f626410ae70068ffa1f53b
Cheryl Murphy <6191116+cherylli@users.noreply.github.com>
2026-07-25 23:07:36 +1000

    update some roles page styles

--- src/components/roles/header-badge.tsx
+++ src/components/roles/header-badge.tsx
@@ -1,21 +1,34 @@
+import { cn } from "#/lib/utils.ts";
 import { Badge } from "@/components/ui/badge";
 
-export const RolesHeaderBadge = ( {
-        text,
-        variant = "section",
-        cornerSize = "sm"
-    }: {
-        text: string,
-        variant?: "hero" | "section",
-        cornerSize?: "sm" | "md" | "lg" | "full"
-    }) =>{
-    return <Badge
-        variant="outline"
-        className={`rounded-${cornerSize} font-bold p-4 uppercase text-primary border-primary/30 bg-primary/10`}
-    >
-        {variant === "hero" &&
-            <div className="w-1.5 h-1.5 mr-2 rounded-full bg-primary"></div>
-        }
-        {text}
-    </Badge>
-}
+const baseBadgeStyles = `
+    p-4
+    font-bold
+    uppercase text-primary 
+    border-primary/30 
+    bg-primary/10 
+    self-start 
+    md:self-center
+`
+
+export const RolesHeaderBadge = ({
+	text,
+	variant = "section",
+	cornerSize = "sm",
+}: {
+	text: string;
+	variant?: "hero" | "section";
+	cornerSize?: "sm" | "md" | "lg" | "full";
+}) => {
+	return (
+		<Badge
+			variant="outline"
+			className={cn(baseBadgeStyles, `rounded-${cornerSize}`)}
+		>
+			{variant === "hero" && (
+				<div className="w-1.5 h-1.5 mr-2 rounded-full bg-primary"></div>
+			)}
+			{text}
+		</Badge>
+	);
+};
