463f72295875e47fe2d925dd456df86c2697f71c
Cheryl Murphy <6191116+cherylli@users.noreply.github.com>
2026-09-07 22:21:31 +1000

    fix: logo link

--- src/components/shared/layout/nav/nav-logo.tsx
+++ src/components/shared/layout/nav/nav-logo.tsx
@@ -1,8 +1,12 @@
+import { Link } from "@tanstack/react-router";
+
 export function NavLogo() {
 	return (
-		<div className="flex items-center py-4">
-			<img src="/images/chingu-logo.svg" alt="logo" className="h-10 w-10" />
-			<span className={`font-extrabold text-2xl ml-2`}>Chingu</span>
-		</div>
+		<Link to="/">
+			<div className="flex items-center py-4 cursor-pointer">
+				<img src="/images/chingu-logo.svg" alt="logo" className="h-10 w-10" />
+				<span className={`font-extrabold text-2xl ml-2`}>Chingu</span>
+			</div>
+		</Link>
 	);
 }
